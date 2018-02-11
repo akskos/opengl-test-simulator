4380e03a0110488bb119912bf30a8faa80103268
Akseli Koskinen <koskinen.akseli@gmail.com>
2018-02-11 18:51:55 +0200

    move the triangle

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,17 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/transform.hpp>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string>
+#include <iostream>
 
 #include "program.h"
 #include "shader.h"
 
-const GLfloat vertex_data[] = {
-    -1.0f, -1.0f, 0.0f,
-     1.0f, -1.0f, 0.0f,
-     0.0f,  1.0f, 0.0f
-};
+using namespace std;
 
 const GLfloat color_data[] = {
     0.0f, 0.5f, 1.0f,
@@ -19,13 +19,16 @@ const GLfloat color_data[] = {
     0.0f, 0.5f, 1.0f
 };
 
+float xcoord = -1.0;
+float xdelta = 0.1;
+
 int main() {
     GLFWwindow *window;
     if (!glfwInit()) {
 	return 1;
     }
 
-    window = glfwCreateWindow(640, 480, "Hello", NULL, NULL);
+    window = glfwCreateWindow(500, 500, "Hello", NULL, NULL);
     if (!window) {
 	glfwTerminate();
 	return 2;
@@ -46,8 +49,6 @@ int main() {
 
     GLuint vbo;
     glGenBuffers(1, &vbo);
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertex_data), vertex_data, GL_STATIC_DRAW);
 
     GLuint cbo;
     glGenBuffers(1, &cbo);
@@ -59,10 +60,31 @@ int main() {
     Program program(vertexShader.getShaderId(), fragmentShader.getShaderId());
     GLuint programId = program.getProgramId();
 
+    double lastTime = glfwGetTime();
     while (!glfwWindowShouldClose(window)) {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	glUseProgram(programId);
+
+	double time = glfwGetTime();
+	double interval = time - lastTime;
+
+	// change xcoord
+	xcoord += xdelta * interval;
+	if (xcoord > 0.5 || xcoord < -1.0) {
+	    xdelta = -xdelta;
+	}
+
+	glm::mat4 matrix;
+	glm::mat4 vector;
+
+	const GLfloat vertex_data[] = {
+	    xcoord, -1.0f, 0.0f,
+	     1.0f, -1.0f, 0.0f,
+	     0.0f,  1.0f, 0.0f
+	};
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertex_data), vertex_data, GL_STATIC_DRAW);
 
 	glEnableVertexAttribArray(0);
 	glEnableVertexAttribArray(1);
@@ -76,6 +98,8 @@ int main() {
 
 	glfwSwapBuffers(window);
 	glfwPollEvents();
+
+	lastTime = glfwGetTime();
     }
 
     glfwTerminate();
