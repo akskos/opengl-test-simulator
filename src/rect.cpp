78d5aa36c02eee88ff39f0adfb5d9cba18b4eed0
Akseli Koskinen <koskinen.akseli@gmail.com>
2018-03-22 23:24:32 +0200

    convert quads to triangles in rect

--- src/rect.cpp
+++ src/rect.cpp
@@ -7,7 +7,7 @@ Rect::Rect(
 	glm::vec3 point4) {
 
     // init vertex data
-    GLfloat vertex_data[4 * 3];
+    GLfloat vertex_data[6 * 3];
     vertex_data[0] = point1.x;
     vertex_data[1] = point1.y;
     vertex_data[2] = point1.z;
@@ -17,9 +17,15 @@ Rect::Rect(
     vertex_data[6] = point3.x;
     vertex_data[7] = point3.y;
     vertex_data[8] = point3.z;
-    vertex_data[9] = point4.x;
-    vertex_data[10] = point4.y;
-    vertex_data[11] = point4.z;
+    vertex_data[9] = point1.x;
+    vertex_data[10] = point1.y;
+    vertex_data[11] = point1.z;
+    vertex_data[12] = point3.x;
+    vertex_data[13] = point3.y;
+    vertex_data[14] = point3.z;
+    vertex_data[15] = point4.x;
+    vertex_data[16] = point4.y;
+    vertex_data[17] = point4.z;
 
     // generate and init vertex buffer
     glGenBuffers(1, &vbo);
@@ -33,6 +39,8 @@ Rect::Rect(
 
     // init color data
     GLfloat color_data[] = {
+	1.0f, 1.0f, 1.0f,
+	1.0f, 1.0f, 1.0f,
 	1.0f, 1.0f, 1.0f,
 	1.0f, 1.0f, 1.0f,
 	1.0f, 1.0f, 1.0f,
@@ -57,7 +65,7 @@ void Rect::render() {
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, 0);
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
-    glDrawArrays(GL_QUADS, 0, 4);
+    glDrawArrays(GL_TRIANGLES, 0, 6);
     glDisableVertexAttribArray(1);
     glDisableVertexAttribArray(0);
 }
