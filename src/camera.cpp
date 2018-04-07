ca84b0931962414489eea91ca2adc5ffbf61d796
Akseli Koskinen <koskinen.akseli@gmail.com>
2018-04-07 16:18:11 +0300

    disable flying

--- src/camera.cpp
+++ src/camera.cpp
@@ -33,13 +33,17 @@ void Camera::instaHorizRotate(double delta) {
 void Camera::instaVertiRotate(double delta) {
     glm::vec3 rotationAxis = glm::cross(front, up);
     glm::mat4 rotationMat = glm::rotate(glm::mat4(1), (float)-delta, rotationAxis);
-    front = glm::vec3(rotationMat * glm::vec4(front, 1.0f));
+    glm::vec3 newFront = glm::vec3(rotationMat * glm::vec4(front, 1.0f));
+    if (newFront.y > -0.99 && newFront.y < 0.99) {
+	front = newFront;
+    }
 }
 
 void Camera::update(double interval) {
     glm::vec3 deltaVec1;
+    glm::vec3 movingFront = glm::normalize(glm::vec3(front.x, 0, front.z));
     if (positionDelta.z != 0) {
-	deltaVec1 = front * (float)interval * positionDelta.z;
+	deltaVec1 = movingFront * (float)interval * positionDelta.z;
     }
     glm::vec3 deltaVec2;
     if (positionDelta.x != 0) {
