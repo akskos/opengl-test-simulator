7a624b67e5e7d6e5f1cbbea0069ab4ab8ce3366e
Akseli <koskinen.akseli@gmail.com>
2018-07-08 20:18:15 +0300

    load shaders and update mvp in graphics class

--- src/Graphics.cpp
+++ src/Graphics.cpp
@@ -1,9 +1,19 @@
 #include "Graphics.h"
 
+using namespace std;
+
 Graphics::Graphics() {}
 
 bool Graphics::init() {
-    return initGlew();
+    if (!initGlew()) {
+        cout << "Failed to init glew" << endl;
+        return false;
+    }
+    if (!loadShaders()) {
+        cout << "Failed to load shaders" << endl;
+        return false;
+    }
+    return true;
 }
 
 bool Graphics::initGlew() {
@@ -11,8 +21,23 @@ bool Graphics::initGlew() {
     if (glewInit() != GLEW_OK) {
         return false;
     }
+    return true;
+}
+
+bool Graphics::loadShaders() {
+    Shader vertexShader(GL_VERTEX_SHADER, "src/vertex.shader");
+    Shader fragmentShader(GL_FRAGMENT_SHADER, "src/fragment.shader");
+    Program program(vertexShader.getShaderId(), fragmentShader.getShaderId());
+    GLuint programId = program.getProgramId();
+    matrixId = glGetUniformLocation(programId, "mvp");
+    glUseProgram(programId);
+    return true;
 }
 
 void Graphics::clear() {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+}
+
+void Graphics::updateMVP(glm::mat4 mvp) {
+    glUniformMatrix4fv(matrixId, 1, GL_FALSE, &mvp[0][0]);
 }
--- src/Graphics.h
+++ src/Graphics.h
@@ -6,15 +6,23 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtx/transform.hpp>
+#include <iostream>
+
+#include "shader.h"
+#include "program.h"
 
 class Graphics {
 public:
     Graphics();
     bool init();
     void clear();
+    void updateMVP(glm::mat4 mvp);
 
 private:
     bool initGlew();
+    bool loadShaders();
+
+    GLuint matrixId;
 };
 
 #endif
